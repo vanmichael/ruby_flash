85df80598507ae50db8ac3776e0b5ec80ff4e7d3
Van Michael <van.michael.nguyen@gmail.com>
2013-12-24 12:20:10 -0500

    first commit and gem install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyFlash::Application.config.secret_key_base = '9e601a75dcad257fc9b7be42ec0b4583beb46942410d46d5e7884c60935cf3aa2f9f80086477941867380f272d1d9a2a2a96dd2e8c43d24a7aeee13f606e040e'
