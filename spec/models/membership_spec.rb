e78d84bc65c015caa4f76997b5da98eca21f19b9
Van Michael <van.michael.nguyen@gmail.com>
2014-01-26 17:36:52 -0500

    add two feature tests

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe Membership do
   #Validations
   it { should have_valid(:user_id).when(1) }
-  #it { should_not have_valid(:user_id).when(nil,'') }
+  it { should_not have_valid(:user_id).when(nil,'') }
 
   it { should have_valid(:group_id).when(1) }
-  #it { should_not have_valid(:group_id).when(nil,'') }
+  it { should_not have_valid(:group_id).when(nil,'') }
 
   #associations
 
