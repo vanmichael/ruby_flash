b9fbc23659ed4d508c1883b575a493f00a6b2e34
Van Michael <van.michael.nguyen@gmail.com>
2014-01-26 15:38:18 -0500

    clean up css

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -34,7 +34,7 @@ feature 'user signs in', %q{
   	fill_in 'Email', with: 'van@gmail.com'
   	fill_in 'Password', with: 'password'
   	click_button 'Sign In'
-  	
+
   	expect(page).to have_content('Invalid email or password.')
   end
 
