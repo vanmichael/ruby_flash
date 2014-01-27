4adae713d71e3cded2909936440ff989b608aa4e
Van Michael <van.michael.nguyen@gmail.com>
2014-01-26 22:04:39 -0500

    fix javascript 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// inside app/assets/javascripts/application.js
 //= require jquery
 //= require jquery_ujs
 //= require foundation
